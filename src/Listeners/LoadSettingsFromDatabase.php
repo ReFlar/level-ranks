f10c88aa9a099c0344f65678d91775746092ce1d
David Sevilla Martín <6401250+datitisev@users.noreply.github.com>
2020-05-06 11:56:06 -0400

    Update namespace capitalization

--- src/Listeners/LoadSettingsFromDatabase.php
+++ src/Listeners/LoadSettingsFromDatabase.php
@@ -11,7 +11,7 @@
  * file that was distributed with this source code.
  */
 
-namespace ReFlar\LevelRanks\Listeners;
+namespace Reflar\LevelRanks\Listeners;
 
 use Flarum\Api\Event\Serializing;
 use Flarum\Api\Serializer\ForumSerializer;
